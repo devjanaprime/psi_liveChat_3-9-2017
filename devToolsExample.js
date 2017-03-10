06ee86784fe7d25e3f887378366e1a40a5ec375f
devjanaprime <dev@primeacademy.io>
2017-03-09 18:00:14 -0600

    first

---
+++ devToolsExample.js
@@ -0,0 +1,10 @@
+console.log( 'js' );
+
+function theTripler( startNumber ){
+  console.log( 'in theTripler', startNumber );
+  return startNumber * 3;
+}
+
+theTripler( 9 );
+
+var theOtherNumberThing = 81;
